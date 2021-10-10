7aebb697f08590cbfc069c23b46aef529cc4e756
kazumasasama <72055437+kazumasasama@users.noreply.github.com>
2021-10-10 09:20:32 -0400

    bootstrap導入

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,11 @@
 <template>
   <div id="app">
     <div id="nav">
+      <button type="button" class="btn btn-primary">Primary</button>
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
