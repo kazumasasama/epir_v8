69a6492dc1bdaa307c20b6d3cbc447467c67a0b3
kazumasasama <72055437+kazumasasama@users.noreply.github.com>
2021-10-10 11:28:49 -0400

    Header

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,14 +1,20 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <button type="button" class="btn btn-primary">Primary</button>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-      <router-link to="/event">ご予約</router-link>
-    </div>
+    <Header />
     <router-view />
   </div>
 </template>
+
+<script>
+import Header from "@/components/Header.vue";
+
+export default {
+  name: "App",
+  components: {
+    Header,
+  },
+};
+</script>
 
 <style>
 #app {
---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <b-navbar type="light" variant="light">
+      <div class="container">
+        <div class="logo">
+          <img alt="epiR logo" src="../assets/epiRLogo70x40.png" />
+        </div>
+        <b-navbar-nav class="nav">
+          <b-nav-item class="nav-item1">
+            <router-link to="/">メニュー選択</router-link> |
+            <router-link to="/event">ご予約カレンダー</router-link>
+          </b-nav-item>
+          <!-- Navbar dropdowns -->
+          <b-nav-item-dropdown text="言語設定" right>
+            <b-dropdown-item @click="switchToEn">ENG</b-dropdown-item>
+            <b-dropdown-item @click="switchToJa">日本語</b-dropdown-item>
+          </b-nav-item-dropdown>
+
+          <b-nav-item-dropdown text="アカウント" right>
+            <b-dropdown-item href="#">Account</b-dropdown-item>
+            <b-dropdown-item href="#">Settings</b-dropdown-item>
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
+      </div>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    switchToEn() {
+      this.$emit("switchToEn");
+    },
+    switchToJa() {
+      this.$emit("switchToJa");
+    },
+  },
+};
+</script>
+
+<style>
+.nav {
+  padding: 15px;
+  text-align: right;
+}
+.nav-item1 {
+  margin-left: 20px;
+}
+</style>
